b0bdebdcfba6447bfa82bea05ee89c9c259fdd13
bikach <chakibhoud.io@gmail.com>
2019-06-23 23:29:47 +0200

    update MainFactory

--- src/Creational/factory/MainFactory.java
+++ src/Creational/factory/MainFactory.java
@@ -3,10 +3,10 @@ package Creational.factory;
 public class MainFactory {
 
     public static void main(String[] args) {
-        Website site = WebsiteFactory.getWebsite("blog");
+        Website site = WebsiteFactory.getWebsite(WebsiteType.BLOG);
         System.out.println(site.getPages());
 
-        site = WebsiteFactory.getWebsite("Shop");
+        site = WebsiteFactory.getWebsite(WebsiteType.SHOP);
         System.out.println(site.getPages());
     }
-}
+}x
