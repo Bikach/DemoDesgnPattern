280af5d5264a16752bf03e7ccbeee8526a7c0634
bikach <chakibhoud.io@gmail.com>
2019-06-23 17:01:57 +0200

    Add example Calendar factory

---
+++ src/Creational/factory/CalendarFactoryDemo.java
@@ -0,0 +1,18 @@
+package Creational.factory;
+
+import java.util.Calendar;
+
+public class CalendarFactoryDemo {
+
+    public static void main(String[] args) {
+
+        Calendar cal = Calendar.getInstance();
+        //Calendar cal = Calendar.get
+
+        System.out.println(cal);
+
+        System.out.println(cal.get(Calendar.DAY_OF_MONTH));
+
+
+    }
+}
