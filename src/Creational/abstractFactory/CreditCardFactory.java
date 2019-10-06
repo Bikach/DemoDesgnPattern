e9325466bcd1a94ba1002bdcfd8605427389f3ee
Bikach <chakibhoud.io@gmail.com>
2019-10-06 18:18:40 +0200

    clean up project

--- src/Creational/abstractFactory/CreditCardFactory.java
+++ src/Creational/abstractFactory/CreditCardFactory.java
@@ -4,13 +4,10 @@ package Creational.abstractFactory;
 public abstract class CreditCardFactory {
 
 	static CreditCardFactory getCreditCardFactory(int creditScore) {
-	
 		if(creditScore > 650) {
 			return new AmexFactory();
 		}
-		else {
-			return new VisaFactory();
-		}
+		return new VisaFactory();
 	}
 	
 	public abstract CreditCard getCreditCard(CardType cardType);
--- src/Creational/abstractFactory/VisaFactory.java
+++ src/Creational/abstractFactory/VisaFactory.java
@@ -6,15 +6,10 @@ public class VisaFactory extends CreditCardFactory {
 	public CreditCard getCreditCard(CardType cardType) {
 		switch (cardType) {
 			case GOLD:
-				
 				return new VisaGoldCreditCard();
-	
 			case PLATINUM:
-				
-				return new VisaBlackCreditCard();	
-			
+				return new VisaBlackCreditCard();
 		}
-		
 		return null;
 	}
 
