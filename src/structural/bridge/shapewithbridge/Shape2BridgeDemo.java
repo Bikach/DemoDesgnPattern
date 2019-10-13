a0a4d4ad42fca5c534abed6f7527caa00309c2d2
Bikach <chakibhoud.io@gmail.com>
2019-10-13 11:58:26 +0200

    rename both circule and square object from main class

--- src/structural/bridge/shapewithbridge/Shape2BridgeDemo.java
+++ src/structural/bridge/shapewithbridge/Shape2BridgeDemo.java
@@ -4,21 +4,17 @@ public class Shape2BridgeDemo {
 
 	public static void main(String[] args) {
 		Color blue = new Blue();
-		
-		Shape square = new Square(blue);
-		
+		Shape blueSquare = new Square(blue);
+
 		Color red = new Red();
-		
-		Shape circle = new Circle(red);
+		Shape redCircle = new Circle(red);
 		
 		Color green = new Green();
-		
 		Shape greenCircle = new Circle(green);
-		
 		Shape greenSquare = new Square(green);
 		
-		square.applyColor();
-		circle.applyColor();
+		blueSquare.applyColor();
+		redCircle.applyColor();
 		greenCircle.applyColor();
 		greenSquare.applyColor();
 	}
